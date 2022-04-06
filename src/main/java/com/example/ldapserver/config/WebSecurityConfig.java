d4c1e82b1eabbcda000d21490e3da7d087b7b33c
Matej26 <matveyvasyra26@gmail.com>
2022-04-06 15:23:29 +0300

    initial commit

---
+++ src/main/java/com/example/ldapserver/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.ldapserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .ldapAuthentication()
+                .userDnPatterns("uid={0}, ou=people")
+                .contextSource()
+                .url("ldap://localhost:8389/dc=com,dc=example")
+                .and()
+                .passwordCompare().passwordAttribute("userPassword");
+    }
+}
